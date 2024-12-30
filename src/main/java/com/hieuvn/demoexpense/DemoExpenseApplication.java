339b0a9501d1f4ceb159c0775e4d0a37403ba0fe
Hieuvn <nthieu9208@gmail.com>
2024-12-30 14:55:44 +0700

    intial commit

---
+++ src/main/java/com/hieuvn/demoexpense/DemoExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.hieuvn.demoexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExpenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExpenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hieuvn/demoexpense/DemoExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieuvn.demoexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExpenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
